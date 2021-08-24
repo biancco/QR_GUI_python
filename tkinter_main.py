3a0442d35b7f2346b0b055f03754194205fc80b2
dongkwan <biancco@snu.ac.kr>
2021-08-25 00:40:21 +0900

    gif

--- tkinter_main.py
+++ tkinter_main.py
@@ -2,11 +2,17 @@ from tkinter import *
 
 class Myframe(Frame):
     def __init__(self, master):
-        img = PhotoImage(file='test.jpg')
+        img = PhotoImage(file='test.gif')
         lbl = Label(image=img)
+        lbl.image = img  #img객체를 계속 살려두는 역할
         lbl.grid(row=0,column=0)
 
 def main():
     root = Tk()
     root.title("이미지 보기")
-    root.geometry()
+    root.geometry('500x400+100+100')
+    myframe = Myframe(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
